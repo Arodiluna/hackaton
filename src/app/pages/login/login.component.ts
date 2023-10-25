f1593288630fa0b2eea395be58daf9a8e841906f
Arodi Luna <arodilunacor@gmail.com>
2023-10-25 11:38:32 -0600

    Login, guards
    Login y guards se integraron y se valida el token.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,82 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  usuario = {
+    email: '',
+    pass: ''
+  }
+
+  alerta = {
+    informacion: '',
+    error: '',
+    correcto: ''
+  }
+
+  loading: boolean = false;
+
+
+  //Llamar tokern session.
+  token = localStorage.getItem('token');
+
+  constructor ( private loginService: LoginService,
+                private route: Router ) { }
+
+  ngOnInit() {
+     //Verificar token.
+     if ( this.token == this.token ) {
+      this.route.navigate(['home']);
+     } else {
+      this.route.navigate(['login']);
+     }
+  }
+
+  //Email no permitir espacios.
+  removeSpaces() {
+    this.usuario.email = this.usuario.email.replace(/\s+/g, '').trim();
+  }
+
+  //Convertir en mínusculas
+  convertToLower(): void {
+    this.usuario.email = this.usuario.email.toLowerCase();
+  }
+
+  async login() {
+    console.log(this.usuario);
+    const contador = timer(5000);
+    if ( this.usuario.pass == '' || this.usuario.email == '') {
+      console.log('Campos vacíos');
+      this.alerta.informacion = 'Hay campos vacíos';
+      contador.subscribe((n) => {
+        this.alerta.informacion= '';
+      });
+    } else {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+      if ( !emailRegex.test( this.usuario.email ) ) {
+        this.alerta.error = 'Email no válido';
+        console.log('Emila no valido');
+        contador.subscribe((n) => {
+          this.alerta.error = '';
+        });
+      } else {
+        this.loading = true;
+        const valido = await this.loginService.loginAdm( this.usuario.email, this.usuario.pass );
+
+        if ( valido ) {
+          this.loading = false;
+          this.alerta.correcto = 'Inicio de sesión correcto';
+          contador.subscribe((n) => {
+            this.alerta.correcto = '';
+          });
+        } else {
+          this.loading = false;
+          this.alerta.error = 'Inicio de sesión incorrecto, volver a intentar';
+          contador.subscribe((n) => {
+            this.alerta.error = '';
+          });
+        }
+      }
+    }
+  }
 
 }
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,105 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+
+//URL.
+const URL = environment.url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor( private http: HttpClient, 
+    private route: Router ) { }
+
+    loginAdm( email: string, pass: string ): Promise<boolean> {
+
+      //Constante con el valor email y pass.
+      const data = { email, pass };
+  
+      //Crear promesa.
+      return new Promise<boolean>( resolve => {
+        //Suscribe al login.
+      this.http.post(`${ URL }/api/auth/login`, data)
+      .pipe(
+        //Mostrar alerta de error / eliminar local storage
+        catchError((error: HttpErrorResponse) => {
+          if (error.ok === false) {
+            //Dejar de funcionar loader.
+            localStorage.removeItem('token');
+            resolve(false);
+          } 
+          return throwError(() => error);
+        })
+      )
+      .subscribe( resp => {
+        const jsonResp = JSON.parse(JSON.stringify(resp));
+  
+        console.log(resp);
+
+        if ( jsonResp.ok ) {
+          console.log(jsonResp);
+          //Guardar token.
+          localStorage.setItem('token', jsonResp.token);
+        
+          //Guardar roles y dar valores string.
+          if ( jsonResp.discapacidad == '1' ) {
+            //jsonResp.rol = 'admin';
+            localStorage.setItem('rol', 'lKjhw8h2$22lajJALmuenakkd9278LjASDLEWNNWI%&ddh&');
+          } else if (jsonResp.discapacidad == '2') {
+            //jsonResp.rol = 'manager';
+            localStorage.setItem('rol', '#2jsfkslll99lladskMngi81#sdkfsiikKJOIWOIE9983$jaoid');
+          } 
+          //Enviar a home.
+          this.route.navigate(['/home']);
+          
+          //Resolve true.
+          resolve(true);
+        } else {
+          localStorage.removeItem('token');
+          resolve(false);
+        }
+  
+      }); 
+    }); 
+  }
+
+  //Renovar token.
+validarToken (): Promise<boolean> {
+
+  return new Promise<boolean> ( resolve => {
+    const headers = new HttpHeaders({
+      'x-token': localStorage.getItem('token') || ''
+    });
+    this.http.get(`${ URL }/api/auth/token`, { headers })
+    .pipe(
+      //Mostrar alerta de error / eliminar local storage
+      catchError((error: HttpErrorResponse) => {
+        if (error.ok === false) {
+          this.route.navigate(['login']);
+          localStorage.removeItem('token');
+          resolve(false);
+        } 
+        return throwError(() => error);
+      })
+    )
+    .subscribe( resp => {
+      //Convertir en JSON.
+      const respuesta = JSON.parse(JSON.stringify(resp));
+      if (respuesta.ok) {
+        //Mostrar info usuario.
+        const usuario = respuesta;
+        localStorage.setItem('token', usuario.token);
+        //console.log(usuario.id_usuario);
+        resolve(true);
+      } else {
+        resolve(false);
+        localStorage.removeItem('token');
+      }
+    })
+  });
+}
 }
