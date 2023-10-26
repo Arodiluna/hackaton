f2a64226fc041ceba093759eeae77df271a7486c
Arodi Luna <arodilunacor@gmail.com>
2023-10-26 10:40:53 -0600

    Ia acabada
    Se terminó la IA que genera texto e imagenes

--- src/app/services/datos.service.ts
+++ src/app/services/datos.service.ts
@@ -54,4 +54,81 @@ export class DatosService {
     }); 
   }
 
+  openaialumno( mensaje: string ): Promise<boolean> {
+
+    //Constante con el valor email y pass.
+    const data = { mensaje };
+
+    //Crear promesa.
+    return new Promise<boolean>( resolve => {
+      const headers = new HttpHeaders({
+        'x-token': localStorage.getItem('token') || ''
+      });
+      //Suscribe al login.
+    this.http.post(`${ URL }/api/auth/mensaje`, data, { headers })
+    .pipe(
+      //Mostrar alerta de error / eliminar local storage
+      catchError((error: HttpErrorResponse) => {
+        if (error.ok === false) {
+          //Dejar de funcionar loader.
+          resolve(false);
+        } 
+        return throwError(() => error);
+      })
+    )
+    .subscribe( resp => {
+      const jsonResp = JSON.parse(JSON.stringify(resp));
+
+      if ( jsonResp.ok ) {
+        //Guardar mensaje.
+        localStorage.setItem('mensaje-alumno', jsonResp.mensaje);
+
+        //Resolve true.
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+
+    }); 
+  }); 
+}
+
+  openaiaimagen( mensaje: string ): Promise<boolean> {
+
+    //Constante con el valor email y pass.
+    const data = { mensaje };
+
+    //Crear promesa.
+    return new Promise<boolean>( resolve => {
+      const headers = new HttpHeaders({
+        'x-token': localStorage.getItem('token') || ''
+      });
+      //Suscribe al login.
+    this.http.post(`${ URL }/api/auth/gpt/image`, data, { headers })
+    .pipe(
+      //Mostrar alerta de error / eliminar local storage
+      catchError((error: HttpErrorResponse) => {
+        if (error.ok === false) {
+          //Dejar de funcionar loader.
+          resolve(false);
+        } 
+        return throwError(() => error);
+      })
+    )
+    .subscribe( resp => {
+      const jsonResp = JSON.parse(JSON.stringify(resp));
+      if ( jsonResp.ok ) {
+        //Guardar mensaje.
+        localStorage.setItem('url', jsonResp.url[0].url);
+
+        //Resolve true.
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+
+    }); 
+  }); 
+}
+
 }
